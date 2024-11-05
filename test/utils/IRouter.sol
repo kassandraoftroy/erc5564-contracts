29c01f4cebbc839b659c5b925bfe8bc9a511e93c
κασσάνδρα.eth <0xDADA@protonmail.com>
2024-11-05 10:39:33 +0700

    feat: add StealthSwap test

---
+++ test/utils/IRouter.sol
@@ -0,0 +1,22 @@
+    // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IRouter {   
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    )
+        external
+        payable
+        returns (uint[] memory amounts);
+}
