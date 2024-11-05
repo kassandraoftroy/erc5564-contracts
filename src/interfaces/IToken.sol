ba4002a641d6d03152c305088f31c30c3d570718
κασσάνδρα.eth <0xDADA@protonmail.com>
2024-11-05 08:57:26 +0700

    fix: missing transfers and approvals (#11)
    * fix: missing transfers and approvals in swapper
    
    * fix: errors
    
    * fix: tests

--- src/interfaces/IToken.sol
+++ src/interfaces/IToken.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-interface ITransferFrom {
+interface IToken {
     function transferFrom(address, address, uint256) external;
 
     function balanceOf(address) external returns (uint256);
+
+    function approve(address, uint256) external;
 }
